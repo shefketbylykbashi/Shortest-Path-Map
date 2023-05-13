e754b708fcf5510abd9a1c7f981f369a1138189d
Shefket Bylykbashi <shefketbylykbashi11@gmail.com>
2023-05-13 23:33:37 +0200

    Euclidean formula for calculating the distance between two points on a map , define accept parameters

--- backend/astar.py
+++ backend/astar.py
@@ -13,18 +13,32 @@ class Graph:
         self.node = [] # lista e nyjeve
         self.adj = [[] for i in _node] # krijimi i listes se fqinjesise per secilen nyje
         for n in _node:
-            self.node.append((n['lat'], n['lng'])) #mbushje e listes me te dhenat per nyjet , formati => [(gjatesi ,gjeresia), (gjatesi ,gjeresia)...]
+            self.node.append((n['lat'], n['lng'])) #mbushje e listes me te dhenat per nyjet , formati => [(gjersi,gjatesi), (gjeresia,gjatesi)...]
         for e in _edge:
-            self.adj[e['a']].append((e['b'], self.calcDist(e['a'], e['b']))) #mbushja e listes se fqinjesise me te dhena ,formati [[(),()],[()]]
+            self.adj[e['a']].append((e['b'], self.calcDist(e['a'], e['b']))) #mbushja e listes se fqinjesise me te dhena ,formati [[pernyjenO(destinacioni , pesha),()],[()]]
             self.adj[e['b']].append((e['a'], self.calcDist(e['a'], e['b'])))
-            self.idxStart = _start
-            self.idxEnd = _end
+        self.idxStart = _start
+        self.idxEnd = _end
     
 
     def calcDist(self, a, b):
-        return
+        return 111 * math.sqrt(
+			(self.node[a][0] - self.node[b][0]) * (self.node[a][0] - self.node[b][0]) +
+			(self.node[a][1] - self.node[b][1]) * (self.node[a][1] - self.node[b][1])) 
+    
 
-
-    def astar():
+    #inplementimi i algoritmit astar per shortest path
+    def astar(self):
         return{}
+ 
+ 
+def receive(req):
+	g = Graph(
+		json.loads(req['node']),
+		json.loads(req['edge']),
+		json.loads(req['start']),
+		json.loads(req['end'])
+	) #responsi qe presum 
+	return g.astar()
+
 
