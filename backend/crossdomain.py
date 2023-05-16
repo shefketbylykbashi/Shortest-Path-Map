1c41c8029dfa909f7427abc0d9107fb41b6566aa
selvijebucolli1 <selvije.bucoli87@gmail.com>
2023-05-16 13:24:24 +0200

    crossdomain -preprocesimi i input parametrave

--- backend/crossdomain.py
+++ backend/crossdomain.py
@@ -7,6 +7,15 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
                 attach_to_all=True, automatic_options=True):
     #   Decorator funksion qe lejon crossdomen requests
    
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
     def get_methods():
     #  Tregon cilat metoda lejohen
         if methods is not None:
@@ -18,7 +27,7 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
     def decorator(f):
       
         def wrapped_function(*args, **kwargs):
-        #    Permban crossdomain codin
+        #    Permban crossdomain kodin
             if automatic_options and request.method == 'OPTIONS':
                 resp = current_app.make_default_options_response()
             else:
@@ -29,6 +38,7 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
             h = resp.headers
             h['Access-Control-Allow-Origin'] = origin
             h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
             h['Access-Control-Allow-Credentials'] = 'true'
             h['Access-Control-Allow-Headers'] = \
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization"
@@ -36,6 +46,6 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
                 h['Access-Control-Allow-Headers'] = headers
             return resp
 
-      
+        f.provide_automatic_options = False
         return update_wrapper(wrapped_function, f)
     return decorator
