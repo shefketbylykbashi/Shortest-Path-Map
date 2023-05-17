7ebf1831d8e6a38c44af3fbf4c9bccc39a9ef1bd
Rina Bekolli <94354965+rinabekolli@users.noreply.github.com>
2023-05-17 22:11:51 +0200

    Added dijkstraRoute function

--- backend/index.py
+++ backend/index.py
@@ -3,6 +3,9 @@ from flask import jsonify
 from flask import request
 from crossdomain import *
 import astar
+import dijkstra
+
+app = Flask(__name__)
 
 @app.route('/a-star', methods=['POST', 'OPTIONS', 'GET'])
 @crossdomain(origin='*')
@@ -13,8 +16,15 @@ def astarRoute():
     else:
         return 'ok'
     
+@app.route('/dijkstra', methods=['POST', 'OPTIONS', 'GET'])
+@crossdomain(origin='*')
+def dijkstraRoute():
+    if (request.method == 'POST'):
+        print(request.form)
+        return jsonify(dijkstra.receive(request.form))
+    else:
+        return 'ok'
 
-app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(debug=True)
