8c742cae05e270a188ff33d979064bdf183b9b0d
Rina Bekolli <94354965+rinabekolli@users.noreply.github.com>
2023-05-17 22:09:21 +0200

    Added astarRoute function

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,18 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from crossdomain import *
+import astar
+
+@app.route('/a-star', methods=['POST', 'OPTIONS', 'GET'])
+@crossdomain(origin='*')
+def astarRoute():
+    if (request.method == 'POST'):
+        print(request.form)
+        return jsonify(astar.receive(request.form))
+    else:
+        return 'ok'
+    
 
 app = Flask(__name__)
 
