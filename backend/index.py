5c1a84bf4ba42ebf996a6de1f3dcc4f7ca66df1f
Erna Hulaj <78096644+ernahulaj@users.noreply.github.com>
2023-05-17 22:25:56 +0200

    modify dijkstra method call

--- backend/index.py
+++ backend/index.py
@@ -1,27 +1,27 @@
 from flask import Flask
 from flask import jsonify
-from flask import request
 from crossdomain import *
-import astar
-import dijkstra
+import algorithms
 
 app = Flask(__name__)
+
 
 @app.route('/a-star', methods=['POST', 'OPTIONS', 'GET'])
 @crossdomain(origin='*')
 def astarRoute():
-    if (request.method == 'POST'):
+    if request.method == 'POST':
         print(request.form)
-        return jsonify(astar.receive(request.form))
+        return jsonify(algorithms.receiveAStar(request.form))
     else:
         return 'ok'
-    
+
+
 @app.route('/dijkstra', methods=['POST', 'OPTIONS', 'GET'])
 @crossdomain(origin='*')
 def dijkstraRoute():
-    if (request.method == 'POST'):
+    if request.method == 'POST':
         print(request.form)
-        return jsonify(dijkstra.receive(request.form))
+        return jsonify(algorithms.receiveDijkstra(request.form))
     else:
         return 'ok'
 
